0e2802213cc11efe1c0dc73f220681a5e348c8f8
Milko Daskalov <mdaskalov@outlook.com>
2025-03-16 18:27:39 +0100

    adapted for s3 (not tested)

--- src/ulp_esp32s3.py
+++ src/ulp_esp32s3.py
@@ -11,61 +11,28 @@ source = """\
 
 # constants from:
 # https://github.com/espressif/esp-idf/blob/v5.0.2/components/soc/esp32s3/include/soc/rtc_io_reg.h
-#define RTC_IO_TOUCH_PAD2_REG       (DR_REG_RTCIO_BASE + 0x8C)
-#define RTC_IO_TOUCH_PAD2_MUX_SEL_M (BIT(19))
-#define RTC_GPIO_OUT_REG            (DR_REG_RTCIO_BASE + 0x0)
-#define RTC_GPIO_ENABLE_REG         (DR_REG_RTCIO_BASE + 0xC)
-#define RTC_GPIO_ENABLE_S           10
-#define RTC_GPIO_OUT_DATA_S         10
+#define RTC_GPIO_OUT_REG          (DR_REG_RTCIO_BASE + 0x0)
+#define RTC_GPIO_OUT_DATA_S       10
 
 # constants from:
 # https://github.com/espressif/esp-idf/blob/v5.0.2/components/soc/esp32s3/include/soc/rtc_io_channel.h
-#define RTCIO_GPIO1_CHANNEL         1   //RTCIO_CHANNEL_1
-#define RTCIO_GPIO2_CHANNEL         2   //RTCIO_CHANNEL_2
+#define RTCIO_GPIO17_CHANNEL        17   //RTCIO_CHANNEL_17
+#define RTCIO_GPIO18_CHANNEL        18   //RTCIO_CHANNEL_18
 
 # When accessed from the RTC module (ULP) GPIOs need to be addressed by their channel number
-.set dat, RTCIO_GPIO1_CHANNEL
-.set clk, RTCIO_GPIO2_CHANNEL
-
-.set token, 0xcafe  # magic token
+.set dat, RTCIO_GPIO17_CHANNEL
+.set clk, RTCIO_GPIO18_CHANNEL
 
 .text
   jump entry
 
-magic:   .long 0 # ULP.get_mem(1)
-data_hi: .long 0 # ULP.get_mem(2)
-mask_hi: .long 0 # ULP.get_mem(3)
-data_lo: .long 0 # ULP.get_mem(4)
-mask_lo: .long 0 # ULP.get_mem(5)
+data_lo: .long 0 # ULP.get_mem(1)
+mask_lo: .long 0 # ULP.get_mem(2)
+data_hi: .long 0 # ULP.get_mem(3)
+mask_hi: .long 0 # ULP.get_mem(4)
 
 .global entry
 entry:
-  # load magic flag
-  move r0, magic
-  ld r1, r0, 0
-
-  # test if we have initialised already
-  sub r1, r1, token
-  jump submit, eq  # jump if magic == token (note: "eq" means the last instruction (sub) resulted in 0)
-
-init:
-  # connect GPIO to ULP (0: GPIO connected to digital GPIO module, 1: GPIO connected to analog RTC module)
-  WRITE_RTC_REG(RTC_IO_TOUCH_PAD2_REG, RTC_IO_TOUCH_PAD2_MUX_SEL_M, 1, 1);
-
-  # GPIO shall be output, not input (this also enables a pull-down by default)
-  WRITE_RTC_REG(RTC_GPIO_ENABLE_REG, RTC_GPIO_ENABLE_S + dat, 1, 1)
-  WRITE_RTC_REG(RTC_GPIO_ENABLE_REG, RTC_GPIO_ENABLE_S + clk, 1, 1)
-
-  # reset both GPIOs
-  WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + dat, 1, 0)
-  WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 0)
-
-  # store that we're done with initialisation
-  move r0, magic
-  move r1, token
-  st r1, r0, 0
-
-submit:
   # load mask_hi packet in R0
   move r2, mask_hi
   ld r0, r2, 0
@@ -82,31 +49,34 @@ submit:
   ld r1, r2, 0
 
 start:
-  # 1us approx 8,687755102 cycles
+  # 1µs approx 8,687755102 cycles
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + dat, 1, 0)
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 1)
-  wait 4209
+  wait 4220 # 488µs
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + dat, 1, 1)
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 0)
-  wait 8471
-  WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + dat, 1, 1)
+  wait 8471 # 2*488µs = 976µs
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 1)
-  wait 4209
+  wait 4230 # 488µs
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 0)
+  wait 43
+  jump first_bit
+
 data:
   and r2, r1, r0
   jump off, eq
-on:
+
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + dat, 1, 1)
   jump clock_toggle
 off:
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + dat, 1, 0)
-  jump clock_toggle
+  wait 1
+
 clock_toggle:
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 0)
-  wait 4209
+  wait 4185 # 488µs
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 1)
-  wait 4209
+  wait 4232 # 488µs
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 0)
   rsh r0, r0, 1
   jumpr data, 0, GT
@@ -125,7 +95,26 @@ clock_toggle:
   # load data_lo packet in R1
   move r2, data_lo
   ld r1, r2, 0
-  jump data
+
+first_bit:
+  and r2, r1, r0
+  jump first_bit_off, eq
+
+  WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + dat, 1, 1)
+  jump first_bit_clock_toggle
+
+first_bit_off:
+  WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + dat, 1, 0)
+  wait 1
+
+first_bit_clock_toggle:
+  WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 0)
+  wait 4142 # 488µs
+  WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 1)
+  wait 4232 # 488µs
+  WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + clk, 1, 0)
+  rsh r0, r0, 1
+  jumpr data, 0, GT
 
 stop:
   WRITE_RTC_REG(RTC_GPIO_OUT_REG, RTC_GPIO_OUT_DATA_S + dat, 1, 0)
@@ -135,18 +124,22 @@ stop:
 binary = src_to_binary(source, cpu="esp32s2")
 
 # Export section for Berry
-code_b64 = ubinascii.b2a_base64(binary).decode('utf-8')[:-1]
+code_b64 = ubinascii.b2a_base64(binary).decode("utf-8")[:-1]
 
-print("")
-print("#You can paste the following snippet into Tasmotas Berry console:")
+print("# You can paste the following snippet into Tasmotas Berry console:")
 print("import ULP")
-print("ULP.wake_period(0,1000000) # start")
-print("ULP.wake_period(1,490) # short")
-print("ULP.wake_period(2,975) # long")
-print("ULP.gpio_init(1, 1)")
-print("ULP.gpio_init(2, 1)")
-print("ULP.set_mem(2, 0x30F)")
-print("ULP.set_mem(3, 0x800)")
-print("var c = bytes().fromb64(\""+code_b64+"\")")
+print("ULP.wake_period(0,1000) # update each 1s")
+print("ULP.set_mem(1,0) # data_lo")
+print("ULP.set_mem(2,0) # mask_lo")
+print("ULP.set_mem(3,0) # data_hi")
+print("ULP.set_mem(4,0) # mask_hi")
+print("ULP.gpio_init(17, 1)")
+print("ULP.gpio_init(18, 1)")
+print('var c = bytes().fromb64("' + code_b64 + '")')
 print("ULP.load(c)")
 print("ULP.run()")
+print("# transmit 12:34 (A: 1,2,3,4,0,0) 584C2005 -> 584C:8000 2005:8000")
+print("ULP.set_mem(1,0x2005) # data_lo")
+print("ULP.set_mem(2,0x8000) # mask_lo")
+print("ULP.set_mem(3,0x584C) # data_hi")
+print("ULP.set_mem(4,0x8000) # mask_hi (start)")
